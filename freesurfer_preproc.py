ed3d62cc05f12dc90c3e7100c9c16343a03c8903
Peer-Timo Bremer <bremer5@llnl.gov>
2018-03-11 13:58:59 -0700

    Next version

--- freesurfer_preproc.py
+++ freesurfer_preproc.py
@@ -2,11 +2,18 @@ from sys import argv
 from os.path import exists,join,split,splitext,abspath
 from os import system,mkdir,remove,environ
 from shutil import *
+from glob import glob
+
+
+cortical_dir = "label_cortical"
+vol_dir = "volumes_cortical"
+sub_vol_dir = "volumes_subcortical"
 
 if len(argv) < 3:
     print "Usage: %s <freeSurfer-dir> <T1.nii.gz> [force]" % argv[0]
     exit(0)
 
+T1 = abspath(argv[2])
 output_dir = split(abspath(argv[2]))[0]
 subject = split(output_dir)[1]
 
@@ -28,11 +35,45 @@ if force or not exists(join(output_dir,"mri/orig/001.mgz")):
     system(join(fs_dir,'bin/mri_convert') + " %s %s" % (abspath(argv[2]),join(output_dir,"mri/orig/001.mgz")))
 
 
-system(join(fs_dir,'bin/recon-all') + " -s %s -all -no-isrunning" % subject)
+if force or not exists(join(output_dir,"mri","aparc+aseg.mgz")):
+    system(join(fs_dir,'bin/recon-all') + " -s %s -all -no-isrunning" % subject)
 
 
+if force or not exists(join(output_dir,"T12FA.mat")):
+    system(join(fs_dir,'bin/mri_convert') + " %s %s " % (join(output_dir,"mri","brain.mgz"),abspath(argv[2]))
+    
+if force or not exists(join(output_dir,"FA2T1.mat")):
+    system(join(fs_dir,'bin/flirt') + " -in %s -ref %s -omat %s" % (join(output_dir,"FA.mgz"),abspath(argv[2]),joint(output_dir,"FA2T1.mat"))
+    
+if force or not exists(join(output_dir,"T12FA.mat")):
+    system(join(fs_dir,'bin/convert_xfm') + " -omat %s -inverse %s" % (joint(output_dir,"T12FA.mat"),joint(output_dir,"FA2T1.mat"))
+    
+if not exists(join(output_dir,"EDI")):
+    mkdir(join(output_dir,"EDI"))
+
+
+if force or not exists(join(output_dir,"%s_s2fa/lh_thalamus_s2fa.nii.gz")):
+        
+    if not exists(join(output_dir,"label_cortical")):
+        mkdir(join(output_dir,"label_cortical"))    
+    
+    # extract cortical labels (extralabels) 
+    if force or not exists(join(output_dir,"volumes_subcortical/rh_thalamus.nii.gz")):
+        system(join(fs_dir,'mri_annotation2label') + " --subject %s --hemi rh --annotation aparc --outdir label_cortical" % subject)
+        
+    if force or not exists(join(output_dir,"volumes_subcortical/lh_thalamus.nii.gz")):
+        system(join(fs_dir,'mri_annotation2label') + " --subject %s --hemi lh --annotation aparc --outdir label_cortical" % subject)
+        
+    
+    # extract volume labels (label2vol)
+    for label in glob(join(output_dir,"label_cortical","*.label")):
+        vol_name = splitext(split(label)[1])[0] + ".nii.gz"
+        
+        if force or not exists(join(ouput_dir,"volumes_cortical",vol_name))
+            system(join(fs_dir,'mri_label2vol') + " --label %s --temp %s --identity --o %s" % (label,T1,join(output_dir,"volumes_cortical",vol_name)))
     
     
-            
-
+    # make_subcortical_vols
+    #if force or not exists(join(output_dir,"mri","aseg.nii.mgz"))
+    
 
