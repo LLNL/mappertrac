66460fda15f465b3aad2b8474f8d7a95f42b6aa0
Peer-Timo Bremer <bremer5@llnl.gov>
2018-03-13 11:52:15 -0700

    New sub-scripts

---
+++ maskseeds.py
@@ -0,0 +1,66 @@
+from os import system,mkdir,remove,environ
+from os.path import exists,join,split,splitext,abspath
+from shutil import *
+from glob import glob
+
+
+def maskseeds(root_dir,input_dir,output_dir,low_threshold,high_threshold,high_threshold_thalamus,force):
+    
+    fsl = environ['FSL_DIR']
+    
+    
+    if force and exists(output_dir):
+        rmtree(output_dir)
+        
+    
+    if not exists(output_dir):
+        mkdir(output_dir)
+        
+    # Now create two transformed volumes with threshold 1 and 2
+    print "fslmath %s -thr %f -uthr %f -bin tmp.nii.gz" % (join(root_dir,"FA.nii.gz"),low_threshold,high_threshold)
+    system(join(fsl,bin,"fslmath") + " %s -thr %f -uthr %f -bin tmp.nii.gz" % (join(root_dir,"FA.nii.gz"),low_threshold,high_threshold))
+    system(join(fsl,bin,"fslmath") + " %s -thr %f -uthr %f -bin tmp_thalamus.nii.gz" % (join(root_dir,"FA.nii.gz"),low_threshold,high_threshold_thalamus))
+    
+    for seed in glob(join(input_dir,"*s2fa.nii.gz")):
+        region = split(seed)[0].split(".")[1]
+    
+        if force or not exists(join(output_dir,split(seed)[1])):           
+            if region == "thalamus":
+                print "fslmath  %s -mas tmp_thalamus.nii.gz %s" % (seed, join(output_dir,split(seed)[1]))
+                system(join(fsl,bin,"fslmath") + " %s -mas tmp_thalamus.nii.gz %s" % (seed, join(output_dir,split(seed)[1])))
+            else:
+                print "fslmath  %s -mas tmp.nii.gz %s" % (seed, join(output_dir,split(seed)[1]))
+                system(join(fsl,bin,"fslmath") + " %s -mas tmp.nii.gz %s" % (seed, join(output_dir,split(seed)[1])))
+           
+        
+        
+    remove("tmp_thalamus.nii.gz")
+    remove("tmp.nii.gz")
+    
+    
+def saveallvoxels(root_dir,cortical_dir,subcortical_dir,output_name,force):
+    
+    fsl = environ['FSL_DIR']
+
+    if force:
+        remove(join(root_dir,"cort.nii.gz"))
+        remove(join(root_dir,"subcort.nii.gz"))
+    
+    all_vols = ""
+    for vol in glob(join(cortical_dir,"*.s2fa.nii.gz")):
+        all_vols += " " + vol
+    
+    if force or not exists(join(root_dir,"cort.nii.gz")):
+        system(join(fsl,bin,"find_the_biggest %s %s") % (all_vols,join(root_dir,"cort.nii.gz")))
+        
+    all_vols = ""
+    for vol in glob(join(subcortical_dir,"*.s2fa.nii.gz")):
+        all_vols += " " + vol
+    
+    if force or not exists(join(root_dir,"subcort.nii.gz")):
+        system(join(fsl,bin,"find_the_biggest %s %s") % (all_vols,join(root_dir,"subcort.nii.gz")))
+    
+    if force or not exists(output_name):
+        system(join(fsl,bin,"fslmath") + " %s -add %s %s " % (join(root_dir,"cort.nii.gz"),join(root_dir,"subcort.nii.gz"),output_name))
+        system(join(fsl,bin,"fslmath") + " %s -bin %s" % (output_name,output_name))
+    
