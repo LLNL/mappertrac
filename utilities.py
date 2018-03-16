1619718044879f143131235038f7dad0c56cd496
Peer-Timo Bremer <bremer5@llnl.gov>
2018-03-16 11:54:04 -0700

    Bug fixes

--- utilities.py
+++ utilities.py
@@ -1,18 +1,18 @@
 import time
-from os import system
-from os.path import join,environ
+from os import system,environ
+from os.path import join
 
 
 command_map = {
-    'fslmaths' : join(environ['FSL_DIR'],"bin","fslmaths"),
-    'probtrackx2' : join(environ['FREESURFER_HOME'],"bin","probtrackx2"),
+    'fslmaths' : join(environ['FSLDIR'],"bin","fslmaths"),
+    'probtrackx2' : join(environ['FSLDIR'],"bin","probtrackx2"),
     }
 
 
 def run(exe,arguments):
     
     start = time.time()
-    system(command_map(exe) + " " + arguments)
+    system(command_map[exe] + " " + arguments)
     stop = time.time()
     
     print "Running %s took %d seconds" % (exe, round(stop-start))
