1220909ad134a13b92f59bc06ee5af52093e1ffb
Peer-Timo Bremer <bremer5@llnl.gov>
2018-03-19 11:19:58 -0700

    Test changes

--- qsubEDI.py
+++ qsubEDI.py
@@ -23,7 +23,7 @@ for f1 in files:
 args.close()
 
 pbs_cmd = ("python %s" % argv[1] 
-           + " arguments.list %s 1 15" % argv[2]
+           + " arguments.list %s 1 10" % argv[2]
            + " python %s" % join(split(argv[0])[0],"runEDI.py")
            + " --input"
            + " %s" % split(split(abspath(argv[3]))[0])[0]
