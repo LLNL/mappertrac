995e660230673039b51923fbb9663fd40c7e59c8
Joseph Moon <moon15@llnl.gov>
2018-09-11 17:05:53 -0700

    merge prepTract into bedpostX step

--- s0_runAll.py
+++ s0_runAll.py
@@ -27,12 +27,11 @@ if not exists(abspath("license.txt")) and not exists(join(environ['FREESURFER_HO
     print("Error: Running this software requires a Freesurfer license. Make sure to place license.txt here or in $FREESURFER_HOME.")
     print("You can obtain a license at https://surfer.nmr.mgh.harvard.edu/fswiki/License")
 
-run_default('s1_dti_preproc.py', args, args.input_dir + ' ' + args.output_dir)
+run_default('s1_dtiPreproc.py', args, args.input_dir + ' ' + args.output_dir)
 run_default('s2a_bedpostx.py', args, args.output_dir)
-run_default('s2b_freesurfer_preproc.py', args, args.output_dir)
-run_default('s3_prepTract.py', args, args.output_dir)
-run_default('s4_edi_preproc.py', args, args.output_dir)
-run_default('s5_consensusEDI.py', args, args.output_dir)
+run_default('s2b_freesurfer.py', args, args.output_dir)
+run_default('s3_ediPreproc.py', args, args.output_dir)
+run_default('s4_consensusEDI.py', args, args.output_dir)
 
 if args.output_time:
     printFinish(start_time)
--- s2a_bedpostx.py
+++ s2a_bedpostx.py
@@ -27,23 +27,39 @@ if not exists(FSLDIR):
     exit(0)
 
 bedpostx = join(odir,"bedpostx_b1000")
+bedpostxResults = join(odir,"bedpostx_b1000.bedpostX")
+EDI = join(odir, "EDI")
 
 smart_copy(join(odir,"data_eddy.nii.gz"),join(bedpostx,"data.nii.gz"),args.force)
 smart_copy(join(odir,"data_bet_mask.nii.gz"),join(bedpostx,"nodif_brain_mask.nii.gz"),args.force)
 smart_copy(join(odir,"bvals"),join(bedpostx,"bvals"),args.force)
 smart_copy(join(odir,"bvecs"),join(bedpostx,"bvecs"),args.force)
 
-if args.force or not exists(join(odir,"bedpostx_b1000.bedpostX","dyads3.nii.gz")):
+if args.force or not exists(join(bedpostxResults,"dyads3.nii.gz")):
     
-    if exists(join(odir,"bedpostx_b1000.bedpostX")):
-        rmtree(join(odir,"bedpostx_b1000.bedpostX"))
+    if exists(bedpostxResults):
+        rmtree(bedpostxResults)
             
-    
     if exists(join(FSLDIR,"bedpostx_gpu")):
         run("bedpostx_gpu " + bedpostx + " -NJOBS 4")
     else:
-        # print(bedpostx)
         run("bedpostx " + bedpostx)
+
+if args.force or not exists(join(bedpostxResults, "dyads2_dispersion.nii.gz")):
+    bed_dir = join(odir, "bedpostx_b1000.bedpostX")
+    run("make_dyadic_vectors {} {} {} {}".format(join(bedpostxResults, "merged_th1samples"),
+                                                 join(bedpostxResults, "merged_ph1samples"),
+                                                 join(bedpostxResults, "nodif_brain_mask"),
+                                                 join(bedpostxResults, "dyads1")))
+
+    run("make_dyadic_vectors {} {} {} {}".format(join(bedpostxResults, "merged_th2samples"),
+                                                 join(bedpostxResults, "merged_ph2samples"),
+                                                 join(bedpostxResults, "nodif_brain_mask"),
+                                                 join(bedpostxResults, "dyads2")))
+    rmtree(join(EDI, "bedpostx_b1000.bedpostX"))
+
+if not exists(bedpostxResults):
+    copytree(bedpostxResults, join(EDI, "bedpostx_b1000.bedpostX"))
   
 if args.output_time:
     printFinish(start_time)
