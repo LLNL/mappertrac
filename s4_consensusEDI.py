9d73a37aa6bceb70f394ccaa3a3bfa1f28e330af
Joseph Moon <moon15@llnl.gov>
2018-10-08 16:49:52 -0700

    Parsl-ized s4_consensusEDI

--- s4_consensusEDI.py
+++ s4_consensusEDI.py
@@ -3,13 +3,14 @@ import argparse
 import re
 import sys    
 import os
+import parsl
 from os import system,mkdir,remove,environ
 from subscripts.utilities import *
 from tempfile import *
-
-# edge_list = "listEdgesEDI.txt"
-# PBTK_dir = "PBTKresults_EDI_bedpostx_b1000.bedpostX_withdc"
-# PBTK_dir = "PBTKresults"
+from parsl.config import Config
+from parsl.app.app import python_app, bash_app
+from parsl.executors.ipp import IPyParallelExecutor
+from parsl.executors.threads import ThreadPoolExecutor
 
 parser = argparse.ArgumentParser(description='Preprocess Freesurfer data')
 parser.add_argument('output_dir', help='The directory where the output files should be stored')
@@ -24,6 +25,18 @@ if not exists(args.edge_list):
     raise Exception(args.edge_list + " not found")
 
 start_time = printStart()
+
+config = Config(
+    executors=[
+        ThreadPoolExecutor(
+            max_threads=multiprocessing.cpu_count(),
+            label='local_threads'
+        )
+    ],
+    retries=3,
+    checkpoint_mode = 'dfk_exit'
+)
+parsl.load(config)
 
 odir = abspath(args.output_dir)
 pdir = join(odir, args.pbtk_dir)
@@ -48,7 +61,42 @@ if not exists(join(pdir,"twoway_consensus_edges")):
     mkdir(join(pdir,"twoway_consensus_edges"))
 tmp_dir = mkdtemp()
 
+@python_app
+def subproc(pdir,a_to_b,b_to_a):
+    from subscripts.utilities import run,isFloat
+    from os import remove
+    from os.path import exists,join,splitext,abspath,split
+    input_a_b = join(pdir,a_to_b)
+    input_b_a = join(pdir,b_to_a)
+    output = join(pdir,"twoway_consensus_edges",a_to_b)
+    amax = run("fslstats {} -R | cut -f 2 -d \" \" ".format(input_a_b), print_output=False, working_dir=pdir).strip()
+    if not isFloat(amax):
+        print("fslstats on " + a_to_b + " returns invalid value")
+        continue
+    amax = int(float(amax))
+
+    bmax = run("fslstats {} -R | cut -f 2 -d \" \" ".format(input_b_a), print_output=False, working_dir=pdir).strip()
+    if not isFloat(bmax):
+        print("fslstats on " + b_to_a + " returns invalid value")
+        continue
+    bmax = int(float(bmax))
+    if amax > 0 and bmax > 0:
+        tmp1 = join(pdir, "{}to{}_tmp1.nii.gz".format(a, b))
+        tmp2 = join(pdir, "{}to{}_tmp2.nii.gz".format(b, a))
+        run("fslmaths {} -thrP 5 -bin {}".format(input_a_b, tmp1), working_dir=pdir)
+        run("fslmaths {} -thrP 5 -bin {}".format(input_b_a, tmp2), working_dir=pdir)
+        run("fslmaths {} -add {} -thr 1 -bin {}".format(tmp1, tmp2, output), working_dir=pdir)
+        remove(tmp1)
+        remove(tmp2)
+    else:
+        with open(join(pdir, "zerosl.txt", "a")) as log:
+            log.write(edge)
+            log.write(amax)
+            log.write(bmax)
+
 print("Starting edge consensus\n")
+visited = []
+jobs = []
 with open(args.edge_list) as f:
     n=1
     for edge in f.readlines():
@@ -56,52 +104,25 @@ with open(args.edge_list) as f:
         a, b = edge.strip().split(',', 1)
         a_to_b = "{}to{}.nii.gz".format(a, b)
         b_to_a = "{}to{}.nii.gz".format(b, a)
-        output = join(pdir,"twoway_consensus_edges",a_to_b)
-        pair = join(pdir,"twoway_consensus_edges",b_to_a)
-        if not exists(output) and not exists(pair):
-            print("{} and {}".format(a, b))
-
+        if not a_to_b in visited and not b_to_a in visited:
             if not exists(join(pdir,a_to_b)):
                 print("Edge from " + a + " to " + b + " not found")
                 continue
             if not exists(join(pdir,b_to_a)):
                 print("Edge from " + b + " to " + a + " not found")
                 continue
+            jobs.append(subproc(pdir,a_to_b,b_to_a))
+            visited.append(a_to_b)
+for job in jobs:
+    try:
+        job[1].result()
+    except Exception as e:
+        print("Failed job {}".format(job[0]))
 
-            amax = run("fslstats {} -R | cut -f 2 -d \" \" ".format(a_to_b), print_output=False, working_dir=pdir).strip()
-            if not isFloat(amax):
-                print("fslstats on " + a_to_b + " returns invalid value")
-                continue
-            amax = int(float(amax))
-
-            bmax = run("fslstats {} -R | cut -f 2 -d \" \" ".format(b_to_a), print_output=False, working_dir=pdir).strip()
-            if not isFloat(bmax):
-                print("fslstats on " + b_to_a + " returns invalid value")
-                continue
-            bmax = int(float(bmax))
-            
-            if amax > 0 and bmax > 0:
-                tmp1 = join(tmp_dir, "{}to{}_tmp1.nii.gz".format(a, b))
-                tmp2 = join(tmp_dir, "{}to{}_tmp2.nii.gz".format(b, a))
-                run("fslmaths {} -thrP 5 -bin {}".format(a_to_b, tmp1), working_dir=pdir)
-                run("fslmaths {} -thrP 5 -bin {}".format(b_to_a, tmp2), working_dir=pdir)
-                run("fslmaths {} -add {} -thr 1 -bin {}".format(tmp1, tmp2, output), working_dir=pdir)
-            else:
-                with open(join(pdir, "zerosl.txt", "a")) as log:
-                    log.write(edge)
-                    log.write(amax)
-                    log.write(bmax)
-
-            if n == 1:
-                print("Copy first edge")
-                copy(output, total)
-            else:
-                print("Copy next edge")
-                run("fslmaths {0} -add {1} {1}".format(output, total))
-            n+=1
-    print("Final output summed into {}".format(total))
-
-rmtree(tmp_dir)
+copy(join(pdir,"twoway_consensus_edges",visited.pop_front()), total)
+for a_to_b in visited:
+    output = join(pdir,"twoway_consensus_edges",a_to_b)
+    run("fslmaths {0} -add {1} {1}".format(output, total))
 
 if args.output_time:
     printFinish(start_time)
