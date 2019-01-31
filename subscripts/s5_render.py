f965d40cc978741fd2fb49ceb7446ffe911f03c1
Joseph Moon <moon15@llnl.gov>
2019-01-30 17:08:55 -0800

    More detailed image generation

--- subscripts/s5_render.py
+++ subscripts/s5_render.py
@@ -17,7 +17,6 @@ def s5_2_render_target(params, input_file, output_file, inputs=[]):
     from os.path import splitext,join
     start_time = time.time()
     sdir = params['sdir']
-    format_vtk = join(sdir, 'format_vtk.py')
     run_vtk = join(sdir, 'run_vtk.py')
     output_name = splitext(output_file)[0].strip()
     # vtr_file = output_name + '.vtr'
@@ -38,9 +37,7 @@ def run_s5(params, inputs):
     sdir = params['sdir']
     render_dir = join(sdir, 'render')
     smart_mkdir(render_dir)
-    format_vtk = join(sdir, 'format_vtk.py')
     run_vtk = join(sdir, 'run_vtk.py')
-    copyfile('subscripts/format_vtk.py', format_vtk)
     copyfile('subscripts/run_vtk.py', run_vtk)
     s5_1_future = s5_1_start(params, inputs=inputs)
     s5_2_futures = []
@@ -54,24 +51,3 @@ def run_s5(params, inputs):
             print(output_file)
             s5_2_futures.append(s5_2_render_target(params, input_file, output_file, inputs=[s5_1_future]))
     return s5_3_complete(params, inputs=s5_2_futures)
-
-# if args.render:
-#     import nibabel as nib
-#     import pyevtk
-#     from pyevtk.hl import gridToVTK
-#     for subject in subjects:
-#         sname = subject['sname']
-#         sdir = join(odir, sname)
-#         log_dir = join(sdir,'log')
-#         render_dir = join(sdir, 'render')
-#         smart_mkdir(render_dir)
-#         stdout, idx = get_valid_filepath(join(log_dir, "render.stdout"))
-#         params = {
-#             'sdir': sdir,
-#             'container': container,
-#             'stdout': stdout,
-#         }
-#         for target in open(args.render_targets, 'r').readlines():
-#             # Ugly, but only way I could get OSMesa working
-#             run("python2.7 subscripts/render.py {} {}".format('a', 'b'), params)
-#         update_permissions_base(render_dir, args.group)
