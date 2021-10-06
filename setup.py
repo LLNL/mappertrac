aa9ed9041c825426c6adbea55793032001a1ba2b
Joseph Moon <moon15@llnl.gov>
2021-10-05 17:15:54 -0700

    Fix setuptools data file inclusion

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-import sys
-from os.path import abspath, split, join
-from setuptools import setup, find_packages
+import sys,glob
+from os.path import *
+from setuptools import setup,find_packages
 
 src_path = split(abspath(sys.argv[0]))[0]
 src_path = join(src_path, 'mappertrac')
+
+data_files = [p.replace(src_path + '/', '') for p in 
+    glob.glob(join(src_path, 'data', '**', '*'), recursive=True)]
 
 # ------------------------------------------------------------------------------
 setup(
     name='mappertrac',
     description='Probabilistic tractography for high-performance computing',
-    version='1.0.5',
+    version='1.0.6',
     python_requires='>=3.7',
     author='Joseph Moon',
     author_email='moon15@llnl.gov',
@@ -19,8 +22,7 @@ setup(
         ]
     },
     packages=find_packages(),
-    include_package_data=True,
-    package_data={'mappertrac' : ['data/*'] },
+    package_data={'' : data_files},
     install_requires=['pip>=21.2.4', 'parsl>=1.1.0', 'pytest>=6.2.4', 'pytest-cov>=3.0.0', 'scipy>=1.7.1', 'numpy>=1.21.2', 'gputil>=1.4.0'],
     classifiers=[
         "Development Status :: 4 - Beta",
