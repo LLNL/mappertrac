56e9bddc976ccb76c5af3a24b6b74d4fb0063492
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2022-12-08 17:12:01 -0600

    Update build.sh

--- mappertrac/data/container/build.sh
+++ mappertrac/data/container/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-sudo rm -rf image.sif
-sudo singularity build image.sif recipe.def
+sudo rm -rf ./*.sif
+sudo singularity build fsl_tractography.sif fsl_tractography.def
+sudo singularity build freesurfer-v6.0.0.sif freesurfer.def
+sudo singularity build mrtrix3-v3.0.3.sif mrtrix3.def
