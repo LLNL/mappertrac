536edd7b8c12d37e9cbb004ae27b4845ad17da50
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2023-04-19 13:43:23 -0500

    Update to latest FSL version

--- mappertrac/data/container/build.sh
+++ mappertrac/data/container/build.sh
@@ -7,7 +7,7 @@
 DATALAD=$1
 
 sudo rm -rf ./*.sif
-sudo singularity build fsl-v6.0.5.1.sif fsl_v6.0.5.1.def
+sudo singularity build fsl-v6.0.6.4.sif fsl_v6.0.6.4.def
 sudo singularity build freesurfer-v7.3.2.sif freesurfer_v7.3.2.def
 sudo singularity build mrtrix3-v3.0.3.sif mrtrix3.def
 # If you want to download test data from open-neuro, run with yes
