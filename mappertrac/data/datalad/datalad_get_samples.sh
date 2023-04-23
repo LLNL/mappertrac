05ffe2c455a27cc31ac1ea054cae93b88d80b1de
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2023-04-22 19:22:43 -0500

    Remove unused data from test folder

--- mappertrac/data/datalad/datalad_get_samples.sh
+++ mappertrac/data/datalad/datalad_get_samples.sh
@@ -18,3 +18,16 @@ datalad clone https://github.com/OpenNeuroDatasets/ds000206.git
 cd ds000206
 # get data from MGH
 datalad get sub-THP0001/ses-THP0001MGH1
+mkdir ./ds000206/.ignore/
+mv ./ds000206/sub-THP0001/ses-THP0001MGH1/dwi/sub-THP0001_ses-THP0001MGH1_acq-GD31_run-02_dwi* ./ds000206/.ignore/
+mv ./ds000206/sub-THP0001/ses-THP0001MGH1/dwi/sub-THP0001_ses-THP0001MGH1_acq-GD31_run-03_dwi* ./ds000206/.ignore/
+mv ./ds000206/sub-THP0001/ses-THP0001MGH1/dwi/sub-THP0001_ses-THP0001MGH1_acq-GD31_run-04_dwi* ./ds000206/.ignore/
+mv ./ds000206/sub-THP0001/ses-THP0001MGH1/dwi/sub-THP0001_ses-THP0001MGH1_acq-GD79_run-01_dwi* ./ds000206/.ignore/
+mv ./ds000206/sub-THP0001/ses-THP0001MGH1/dwi/sub-THP0001_ses-THP0001MGH1_acq-GD79_run-02_dwi* ./ds000206/.ignore/
+rm -rf ./ds000206/sub-THP0001/ses-THP0001C*
+rm -rf ./ds000206/sub-THP0001/ses-THP0001DART1
+rm -rf ./ds000206/sub-THP0001/ses-THP0001UW1
+rm -rf ./ds000206/sub-THP0001/ses-THP0001UMN1
+rm -rf ./ds000206/sub-THP0001/ses-THP0001UCI1
+rm -rf ./ds000206/sub-THP0001/ses-THP0001JHU1
+rm -rf ./ds000206/sub-THP0001/ses-THP0001IOWA*
